2a6b60fb33099e78858cca076cf75fb3eb2ba1ac
ramanujsingh <ramanuj1st@gmail.com>
2024-09-28 17:14:10 +0530

    design ready

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,9 +2,20 @@ package com.example;
 
 import io.micronaut.runtime.Micronaut;
 
+import java.util.logging.Logger;
 public class Application {
+  private static final Logger logger = Logger.getLogger(Application.class.getName());
 
     public static void main(String[] args) {
+    logger.info("\n"+"____                     ____  _     _     _           \n" +
+            " / __ \\                   |  _ \\(_)   | |   | |          \n" +
+            "| |  | |_ __   ___ _ __   | |_) |_  __| | __| | ___ _ __ \n" +
+            "| |  | | '_ \\ / _ \\ '_ \\  |  _ <| |/ _` |/ _` |/ _ \\ '__|\n" +
+            "| |__| | |_) |  __/ | | | | |_) | | (_| | (_| |  __/ |   \n" +
+            " \\____/| .__/ \\___|_| |_| |____/|_|\\__,_|\\__,_|\\___|_|   \n" +
+            "       | |                                               \n" +
+            "       |_|   ");
+
         Micronaut.run(Application.class, args);
     }
 }
